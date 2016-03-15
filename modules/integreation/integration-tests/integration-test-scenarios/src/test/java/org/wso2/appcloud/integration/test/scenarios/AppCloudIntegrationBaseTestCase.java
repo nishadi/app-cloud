da00ae1520ed1ae3e181370d2411aa4fbfc687dd
danushkaf <danushkaf@wso2.com>
2016-03-15 08:57:43 +0530

    restructuring test cases

---
+++ modules/integreation/integration-tests/integration-test-scenarios/src/test/java/org/wso2/appcloud/integration/test/scenarios/AppCloudIntegrationBaseTestCase.java
@@ -0,0 +1,103 @@
+package org.wso2.appcloud.integration.test.scenarios;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.appcloud.integration.test.utils.AppCloudIntegrationTestConstants;
+import org.wso2.appcloud.integration.test.utils.AppCloudIntegrationTestUtils;
+import org.wso2.appcloud.integration.test.utils.clients.ApplicationClient;
+import org.wso2.carbon.automation.engine.annotations.ExecutionEnvironment;
+import org.wso2.carbon.automation.engine.annotations.SetEnvironment;
+import org.wso2.carbon.automation.test.utils.common.TestConfigurationProvider;
+
+import java.io.File;
+
+/**
+ * Basic test case to implement things common to all app types.
+ */
+public abstract class AppCloudIntegrationBaseTestCase {
+
+	private static final Log log = LogFactory.getLog(AppCloudIntegrationBaseTestCase.class);
+	protected String defaultAdmin;
+	protected String defaultAdminPassword;
+	protected String defaultAppName;
+	protected String serverUrl;
+	protected String tenantDomain;
+	private String fileName;
+	private String runtimeID;
+	private ApplicationClient applicationClient;
+	
+	public AppCloudIntegrationBaseTestCase(String runtimeID, String fileName){
+		this.runtimeID = runtimeID;
+		this.fileName = fileName;
+	}
+
+	@BeforeClass(alwaysRun = true)
+	public void setEnvironment() throws Exception {
+		defaultAdmin = AppCloudIntegrationTestUtils.getAdminUsername();
+		defaultAdminPassword = AppCloudIntegrationTestUtils.getAdminPassword();
+		defaultAppName = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_NAME_KEY);
+		serverUrl = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.URLS_APPCLOUD);
+		tenantDomain =  AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.DEFAULT_TENANT_TENANT_DOMAIN);
+
+		applicationClient = new ApplicationClient(serverUrl, defaultAdmin, defaultAdminPassword);
+		createApplication();
+	}
+	
+	public void createApplication() throws Exception {
+		//Application details
+		String applicationName = 
+				AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_NAME_KEY);
+		String applicationType = 
+				AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_TYPE_KEY);
+		String applicationRevision = 
+				AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_REVISION_KEY);
+		String applicationDescription = 
+				AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_DESC_KEY);
+		String properties = AppCloudIntegrationTestUtils.getKeyValuePairAsJson(
+				AppCloudIntegrationTestUtils.getPropertyNodes(AppCloudIntegrationTestConstants.APP_PROPERTIES_KEY));
+		String tags = AppCloudIntegrationTestUtils.getKeyValuePairAsJson(
+				AppCloudIntegrationTestUtils.getPropertyNodes(AppCloudIntegrationTestConstants.APP_TAGS_KEY));
+		File uploadArtifact = new File(TestConfigurationProvider.getResourceLocation() + fileName);
+
+		//Application creation
+		ApplicationClient applicationClient = new ApplicationClient(serverUrl, defaultAdmin, defaultAdminPassword);
+		applicationClient.createNewApplication(applicationName, this.runtimeID, applicationType, applicationRevision,
+		                                       applicationDescription, this.fileName, properties, tags, uploadArtifact);
+
+		//Wait until creation finished
+		long timeOutPeriod = AppCloudIntegrationTestUtils.getTimeOutPeriod();
+		int retryCount = AppCloudIntegrationTestUtils.getTimeOutRetryCount();
+		int round = 1;
+		while(round <= retryCount) {
+			try {
+				JSONObject result = applicationClient.getApplicationEvents(applicationName, applicationRevision);
+				log.info("Application current status is : " + result.getString(AppCloudIntegrationTestConstants.PROPERTY_STATUS_NAME));
+				Assert.assertEquals("Application creation failed", AppCloudIntegrationTestConstants.STATUS_RUNNING,
+				                    result.getString(AppCloudIntegrationTestConstants.PROPERTY_STATUS_NAME));
+				break;
+			} catch (Exception e) {
+				Thread.sleep(timeOutPeriod);
+				round++;
+			}
+		}
+	}
+
+	@SetEnvironment(executionEnvironments = { ExecutionEnvironment.PLATFORM})
+	@Test(description = "log test")
+	public void testApplication() throws Exception {
+		log.info("dddddddddddddddddddd");
+	}
+
+
+	@AfterClass(alwaysRun = true)
+	public void cleanEnvironment() throws Exception {
+		String applicationName = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_NAME_KEY);
+		boolean isDeleted = applicationClient.deleteApplication(applicationName);
+		Assert.assertEquals("Application deletion failed", isDeleted, true);
+	}
+}
--- modules/integreation/integration-tests/integration-test-scenarios/src/test/java/org/wso2/appcloud/integration/test/scenarios/MSF4JApplicationTestCase.java
+++ modules/integreation/integration-tests/integration-test-scenarios/src/test/java/org/wso2/appcloud/integration/test/scenarios/MSF4JApplicationTestCase.java
@@ -18,73 +18,15 @@ package org.wso2.appcloud.integration.test.scenarios;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.wso2.appcloud.integration.test.utils.AppCloudIntegrationTestConstants;
 import org.wso2.appcloud.integration.test.utils.AppCloudIntegrationTestUtils;
-import org.wso2.appcloud.integration.test.utils.clients.ApplicationClient;
 
-public class MSF4JApplicationTestCase extends ApplicationTestCase {
+public class MSF4JApplicationTestCase extends AppCloudIntegrationBaseTestCase {
 
 	private static final Log log = LogFactory.getLog(MSF4JApplicationTestCase.class);
 
-	@BeforeClass(alwaysRun = true)
-	public void createApplication() throws Exception {
-		applicationClient = new ApplicationClient(serverUrl, defaultAdmin, defaultAdminPassword);
-		String runtimeID = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.MSS_APP_RUNTIME_ID_KEY);
-		String fileName = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.MSS_APP_FILE_NAME_KEY);
-		createApplication(runtimeID, fileName);
+	public MSF4JApplicationTestCase(){
+		super(AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.MSS_APP_RUNTIME_ID_KEY),
+		      AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.MSS_APP_FILE_NAME_KEY));
 	}
-
-//	@SetEnvironment(executionEnvironments = { ExecutionEnvironment.PLATFORM})
-//    @Test(description = "Create war application using rest api")
-//    public void testTomcatWARApplication() throws Exception {
-//		//Application details
-//		String applicationName = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_NAME_KEY);
-//		String runtimeID = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.MSS_APP_RUNTIME_ID_KEY);
-//		String applicationType = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_TYPE_KEY);
-//		String applicationRevision = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_REVISION_KEY);
-//		String applicationDescription = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_DESC_KEY);
-//		String fileName = AppCloudIntegrationTestUtils.getPropertyValue(
-//				AppCloudIntegrationTestConstants.MSS_APP_FILE_NAME_KEY);
-//		String properties = AppCloudIntegrationTestUtils.getKeyValuePairAsJson(
-//				AppCloudIntegrationTestUtils.getPropertyNodes(AppCloudIntegrationTestConstants.APP_PROPERTIES_KEY));
-//		String tags = AppCloudIntegrationTestUtils.getKeyValuePairAsJson(
-//				AppCloudIntegrationTestUtils.getPropertyNodes(AppCloudIntegrationTestConstants.APP_TAGS_KEY));
-//		File uploadArtifact = new File(TestConfigurationProvider.getResourceLocation() + fileName);
-//
-//		//Application creation
-//		ApplicationClient applicationClient = new ApplicationClient(serverUrl, defaultAdmin, defaultAdminPassword);
-//		applicationClient.createNewApplication(applicationName, runtimeID, applicationType, applicationRevision,
-//		                                       applicationDescription, fileName, properties, tags, uploadArtifact);
-//
-//		//Wait until creation finished
-//		long timeOutPeriod = AppCloudIntegrationTestUtils.getTimeOutPeriod();
-//		int retryCount = AppCloudIntegrationTestUtils.getTimeOutRetryCount();
-//		int round = 1;
-//		while(round <= retryCount) {
-//			try {
-//				JSONObject result = applicationClient.getApplicationEvents(applicationName, applicationRevision);
-//				Assert.assertEquals("Application creation failed", AppCloudIntegrationTestConstants.STATUS_RUNNING,
-//				                    result.getString(AppCloudIntegrationTestConstants.PROPERTY_STATUS_NAME));
-//				break;
-//			} catch (Exception e) {
-//				Thread.sleep(timeOutPeriod);
-//				round++;
-//			}
-//		}
-//
-//		//Delete application
-//		boolean isDeleted = applicationClient.deleteApplication(applicationName);
-//		Assert.assertEquals("Application deletion failed", isDeleted, true);
-//    }
-
-	@AfterClass(alwaysRun = true)
-	public void deleteApplication() throws Exception {
-		String applicationName = AppCloudIntegrationTestUtils.getPropertyValue(AppCloudIntegrationTestConstants.APP_NAME_KEY);
-		boolean isDeleted = applicationClient.deleteApplication(applicationName);
-		Assert.assertEquals("Application deletion failed", isDeleted, true);
-	}
-
 }
